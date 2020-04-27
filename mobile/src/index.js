e75e0b7eed536ccf1f574659c77a2b6ae4438e68
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-04-27 13:57:19 +0100

    Routes with React Navigation

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-console.tron.log('Hello');
+import './config/ReactotronConfig';
+import Routes from './routes';
 
 export default function () {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Hello Ucoffe!</Text>
-    </View>
-  );
+  return <Routes />;
 }
