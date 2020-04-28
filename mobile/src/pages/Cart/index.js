480940ad42f4027aa655e240a94aa053a9e20983
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-04-28 19:48:07 +0100

    Started Cart's Styles (mobile)

--- mobile/src/pages/Cart/index.js
+++ mobile/src/pages/Cart/index.js
@@ -1,8 +1,58 @@
 import React from 'react';
-import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Box,
+  Product,
+  ProductInfo,
+  ProductTitle,
+  ProductDescription,
+  ProductPrice,
+  ProductOrderInfo,
+  ProductAmountControl,
+  ProductAmountButton,
+  ProductAmount,
+  ProductSubtotal,
+  ProductImage,
+  ProductRemove,
+} from './styles';
 
 export default function Cart() {
-  return <View />;
+  return (
+    <>
+      <Container>
+        <Box>
+          <Product>
+            <ProductImage
+              source={{
+                uri:
+                  'https://www.waterymouthcafe.co.nz/wp-content/uploads/2017/07/The-insiders-guide-to-great-coffee-from-the-baristas-at-Watery-Mouth-cafe-Blenheim1-500x500.jpg',
+              }}
+            />
+            <ProductInfo>
+              <ProductTitle>AMERICANO</ProductTitle>
+              <ProductDescription>AMERICANO</ProductDescription>
+              <ProductPrice>$6,50</ProductPrice>
+              <ProductRemove>
+                <Icon name="delete-forever" size={28} color="#999" />
+              </ProductRemove>
+            </ProductInfo>
+            <ProductOrderInfo>
+              <ProductAmountControl>
+                <ProductAmountButton>
+                  <Icon name="remove-circle-outline" size={22} color="#FFF" />
+                </ProductAmountButton>
+                <ProductAmount>100</ProductAmount>
+                <ProductAmountButton>
+                  <Icon name="add-circle-outline" size={22} color="#FFF" />
+                </ProductAmountButton>
+              </ProductAmountControl>
+              <ProductSubtotal>SUBTOTAL</ProductSubtotal>
+            </ProductOrderInfo>
+          </Product>
+        </Box>
+      </Container>
+    </>
+  );
 }
--- mobile/src/pages/Cart/styles.js
+++ mobile/src/pages/Cart/styles.js
@@ -1,3 +1,120 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View``;
+export const EmptyCart = styled.View`
+  background: #3a3b3d;
+  flex: 1;
+`;
+
+export const EmptyBox = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  margin: 20px;
+  border-radius: 3px;
+  background: rgba(0, 0, 0, 0.1);
+  padding: 20px;
+`;
+
+export const Title = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: rgba(0, 0, 0, 0.5);
+  margin: 20px 0 5px;
+`;
+
+export const Message = styled.Text`
+  font-size: 13px;
+  color: rgba(0, 0, 0, 0.35);
+  margin-bottom: 25px;
+`;
+
+export const Button = styled(RectButton)`
+  background: #fff;
+  padding: 8px 12px;
+  border-radius: 3px;
+`;
+
+export const ButtonText = styled.Text`
+  color: #3a3b3d;
+  font-size: 13px;
+  font-weight: bold;
+  margin-right: 5px;
+`;
+
+export const Container = styled.ScrollView`
+  flex: 1;
+  background: #3a3b3d;
+`;
+
+export const Box = styled.View`
+  margin: 20px;
+  border-radius: 3px;
+  background: rgba(0, 0, 0, 0.2);
+  padding: 0;
+  elevation: 2;
+`;
+export const Product = styled.View`
+  flex-direction: row;
+  border-bottom-width: ${(props) => (props.last ? '0' : '1px')};
+  border-bottom-color: #ddd;
+  padding: 15px;
+`;
+
+export const ProductInfo = styled.View`
+  flex-shrink: 1;
+  margin: 0 10px;
+`;
+
+export const ProductTitle = styled.Text`
+  font-size: 13px;
+`;
+
+export const ProductDescription = styled.Text`
+  font-size: 16px;
+  margin: 0 8px;
+  color: #fff;
+`;
+
+export const ProductPrice = styled.Text`
+  font-size: 11px;
+  color: #888;
+  margin-bottom: 5px;
+`;
+
+export const ProductOrderInfo = styled.View`
+  flex-shrink: 0;
+  align-items: flex-end;
+  margin-left: auto;
+`;
+
+export const ProductAmountControl = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ProductAmountButton = styled.TouchableOpacity``;
+
+export const ProductAmount = styled.Text`
+  text-align: center;
+  width: 36px;
+  font-size: 15px;
+`;
+
+export const ProductSubtotal = styled.Text`
+  margin-top: auto;
+  font-size: 15px;
+  font-weight: bold;
+`;
+
+export const ProductImage = styled.Image`
+  width: 70px;
+  height: 70px;
+  align-self: center;
+  border-radius: 100px;
+`;
+
+export const ProductRemove = styled.TouchableOpacity`
+  margin-top: auto;
+  margin-left: -4px;
+`;
