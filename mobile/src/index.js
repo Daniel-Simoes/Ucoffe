9b773ad194f194919e555229850e5dd9dd142077
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-04-29 23:44:23 +0100

    Redux Config & First Reducer to Cart

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
 
 import './config/ReactotronConfig';
 import NavigationService from './services/navigation';
@@ -9,15 +10,19 @@ import Hr from './components/Hr';
 
 import Routes from './routes';
 
+import store from './store';
+
 export default function () {
   return (
-    <>
+
+    <Provider store={store}>
       <StatusBar barStyle="light-content" backgroundColor="#3a3b3d" />
       <Header />
       <Hr />
       <Routes
         ref={(navigatorRef) => NavigationService.setNavigator(navigatorRef)}
       />
-    </>
+      </Provider>
+
   );
 }
