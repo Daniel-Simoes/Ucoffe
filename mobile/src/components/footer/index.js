a35fad6182abb04dd3d824956ba6d7c2cc0d7843
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-04-30 16:38:50 +0100

    Compute all Product's Prices in Cart

--- mobile/src/components/footer/index.js
+++ mobile/src/components/footer/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { formatPrice } from '../../util/format';
 
 import {
   Container,
@@ -12,12 +14,27 @@ import {
 } from './styles';
 
 export default function BottomBar({ buttonTitle, buttonIcon }) {
+  const cart = useSelector(state =>
+    state.cart.map(product => ({
+      ...product,
+      subtotal: formatPrice(product.price * product.amount),
+    }))
+  );
+
+  const total = useSelector(state =>
+    formatPrice(
+      state.cart.reduce((total, product) => {
+        return total + product.price * product.amount;
+      }, 0)
+    )
+  );
+
   return (
     <Container>
       <Wrapper>
         <TotalWrapper>
           <TotalLabel>TOTAL</TotalLabel>
-          <TotalValue>$6.50</TotalValue>
+          <TotalValue>{total}</TotalValue>
         </TotalWrapper>
         <Button>
           <ButtonText>{buttonTitle}</ButtonText>
